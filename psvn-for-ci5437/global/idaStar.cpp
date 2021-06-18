f852eda22b6db2bae8963540a683ccd23c14630a
juan <jaoc1811@gmail.com>
2021-06-17 22:07:30 -0400

    Modified topSpin12-4 and topSpin14-4 pdb and added topSpin17-4 pdb

--- psvn-for-ci5437/global/idaStar.cpp
+++ psvn-for-ci5437/global/idaStar.cpp
@@ -13,6 +13,7 @@ state_t state_aux;
 state_t state_prunning_aux;
 vector<int> path;
 unsigned state_count = 0;
+unsigned bound_state_count;
 
 pair<bool, unsigned> f_bounded_dfs_visit(unsigned bound, unsigned g, unsigned (*heu)(state_t *), int history)
 {
@@ -31,7 +32,8 @@ pair<bool, unsigned> f_bounded_dfs_visit(unsigned bound, unsigned g, unsigned (*
 
     while ((ruleid = next_ruleid(&iter)) >= 0)
     {
-        if( fwd_rule_valid_for_history(history, ruleid) == 0 ) continue;
+        if (fwd_rule_valid_for_history(history, ruleid) == 0)
+            continue;
 
         unsigned cost = g + get_fwd_rule_cost(ruleid);
         copy_state(&state_aux, &state);
@@ -39,6 +41,7 @@ pair<bool, unsigned> f_bounded_dfs_visit(unsigned bound, unsigned g, unsigned (*
 
         if (heu(&state) < infinity)
         {
+            bound_state_count++;
             state_count++;
             path.push_back(ruleid);
             int new_history = next_fwd_history(history, ruleid); // next history for child
@@ -62,8 +65,10 @@ void ida_search(state_t init, unsigned (*heu)(state_t *))
     // search with increasing f-value bounds
     while (true)
     {
-        cout << "Running with bound: " << bound << endl;
+        bound_state_count = 0;
+        cout << "Running with bound: " << bound;
         pair<bool, unsigned> p = f_bounded_dfs_visit(bound, 0, heu, init_history);
+        cout << " (" << bound_state_count << " states generated)" << endl;
         if (p.first)
             return;
         bound = p.second;
@@ -98,28 +103,29 @@ int main()
     printf("\n");
 
     printf("Choose heuristic: \n"
-            "1) manhattan\n"
-            "2) pdb additive\n"
-            "3) pdb max\n"
-            "Selected: ");
+           "1) manhattan\n"
+           "2) pdb additive\n"
+           "3) pdb max\n"
+           "Selected: ");
     char input;
     cin >> input;
 
     clock_t t;
     t = clock();
 
-    switch (input){
-        case '1':
-            ida_search(state_init, manhattan);
-            break;     
-        case '2':
-            load_pdb();
-            ida_search(state_init, pdb_additive);
-            break;
-        case '3':
-            load_pdb();
-            ida_search(state_init, pdb_max);
-            break;
+    switch (input)
+    {
+    case '1':
+        ida_search(state_init, manhattan);
+        break;
+    case '2':
+        load_pdb();
+        ida_search(state_init, pdb_additive);
+        break;
+    case '3':
+        load_pdb();
+        ida_search(state_init, pdb_max);
+        break;
     }
     t = clock() - t;
 
@@ -130,7 +136,7 @@ int main()
     }
     cout << endl;
     cout << "Movements: " << path.size() << endl;
-    printf ("It took: %f seconds.\n",((float)t)/CLOCKS_PER_SEC);
+    printf("It took: %f seconds.\n", ((float)t) / CLOCKS_PER_SEC);
     cout << "States generated: " << state_count << " States" << endl;
 
     return 0;
